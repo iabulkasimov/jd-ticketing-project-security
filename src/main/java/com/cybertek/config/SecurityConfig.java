1aa7c11adeef383ae1aa8a7883491c47356688c7
iabulkasimov <iabulkasimov@gmail.com>
2022-05-09 17:16:19 -0400

    Ticketing Project Security

--- src/main/java/com/cybertek/config/SecurityConfig.java
+++ src/main/java/com/cybertek/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.cybertek.config;
 
+import com.cybertek.service.SecurityService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,7 +11,11 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private SecurityService securityService;
 
+    public SecurityConfig(SecurityService securityService) {
+        this.securityService = securityService;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,7 +36,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                     .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                    .logoutSuccessUrl("/login");
+                    .logoutSuccessUrl("/login")
+                .and()
+                .rememberMe()
+                    .tokenValiditySeconds(120)
+                    .key("cybertekSecret")
+                    .userDetailsService(securityService);
 
 
     }
